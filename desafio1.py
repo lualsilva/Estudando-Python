6801a3f08a3956801b213c4c459f9fd76d9fefff
Luciano Alves da Silva <65561389+lualsilva@users.noreply.github.com>
2020-05-21 16:22:55 -0300

    Adicionei o arquivo via upload
    Escreva um programa que pergunte o nome do usuário e imprima.

---
+++ desafio1.py
@@ -0,0 +1,6 @@
+# Escreva um programa que pergunte o nome do usuário e imprima:
+# "É um prazer te conhecer, (nome digitado)!
+
+nome = input('Qual é o seu nome?\n')
+print('É um prazer em te conhecer, {}!'.format(nome))
+
