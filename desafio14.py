ed1dcf164569a377a981a7f8f77b0f0e3395659c
Luciano Alves da Silva <65561389+lualsilva@users.noreply.github.com>
2020-05-21 17:04:15 -0300

    Adicionei o arquivo via upload
    Converta temperatura de ºC para ºF.

---
+++ desafio14.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converta uma temperatura digitada em ºC e converta
+#para ºF.
+
+c = float(input('Digite a temperatura em ºC: '))
+f = ((9*c)/5)+32
+print('A temperatura de {}ºC corresponde a {}ºF!'.format(c, f))
